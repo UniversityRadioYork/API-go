8f9bb1350ac6c9e3a4616c9155acabcfab1d7063
Skwunk <jordan.cameron.vigo@gmail.com>
2019-11-01 20:52:00 +0000

    Log now outputs to stdout and file. Added log file to .gitignore file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"os"
 	"log"
 	"net/http"
@@ -9,13 +10,14 @@ import (
 )
 
 func main() {
-	f, err := os.OpenFile("APIgo.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	f, err := os.OpenFile("API-go.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
 	if err != nil{
 		log.Println(err)
 	}
 	defer f.Close()
-	log.SetFlags(log.Llongfile)
-	log.SetOutput(f)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	multi :=  io.MultiWriter(f, os.Stdout)
+	log.SetOutput(multi)
 
 	log.Printf("API started")
 
